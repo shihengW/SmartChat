f2ee6a017c853efe6eb7d0f310b773acd4014002
swei <shiheng.wei@hotmail.com>
2019-05-25 08:08:26 +0800

    record on server side

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-var express = require('express');
-var app = express();
 var fs = require('fs');
-var https = require('https').createServer(
-        {
-          key: fs.readFileSync('server.key'),
-          cert: fs.readFileSync('server.cert')
-        },
-        app);
-var io = require('socket.io')(https);
+
 const request = require('request-promise');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // Audio generation.
 var AipSpeechClient = require("baidu-aip-sdk").speech;
@@ -19,10 +14,40 @@ var client = new AipSpeechClient(
 var mp3decoder = require('lame').Decoder;
 var speaker = require('speaker');
 
-app.use(express.static('public'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+
+
+// Import module.
+const AudioRecorder = require('node-audiorecorder');
+ 
+// Options is an optional parameter for the constructor call.
+// If an option is not given the default value, as seen below, will be used.
+const options = {
+  program: `rec`,     // Which program to use, either `arecord`, `rec`, or `sox`.
+  device: null,       // Recording device to use.
+  
+  bits: 16,           // Sample size. (only for `rec` and `sox`)
+  channels: 1,        // Channel count.
+  encoding: `signed-integer`,  // Encoding type. (only for `rec` and `sox`)
+  format: `S16_LE`,   // Encoding type. (only for `arecord`)
+  rate: 16000,        // Sample rate.
+  type: `wav`,        // Format type.
+  
+  // Following options only available when using `rec` or `sox`.
+  silence: 3,         // Duration of silence in seconds before it stops recording.
+  thresholdStart: 0,  // Silence threshold to start recording.
+  thresholdStop: 0.5,   // Silence threshold to stop recording.
+  keepSilence: true   // Keep the silence in the recording.
+};
+// Optional parameter intended for debugging.
+// The object has to implement a log and warn function.
+const logger = console;
+ 
+// Create an instance.
+let audioRecorder = new AudioRecorder(options, logger);
 
 function robochat (text) {
   const textTpl = {
@@ -49,6 +74,7 @@ function robochat (text) {
 function genvoice(text) {
   client.text2audio(text, {spd: 4, per: 3}).then(function(result) {
     if (result.data) {
+
         fs.writeFileSync('temp.mp3', result.data);
         fs.createReadStream('temp.mp3')
             .pipe(new mp3decoder/* must be new */)
@@ -63,29 +89,53 @@ function genvoice(text) {
 }
 
 io.on('connection', function(socket) {
-  socket.on('yousay', function(pcm){
-    client.recognize(pcm, 'wav', 16000, { lan: 'zh' })
-      .then(function(res) {
-        if (res.err_no === 0) {
-          let msg = res.result[0];
-          io.to(socket.id).emit('yousay', msg);
-          robochat(msg)
-            .then((res) => {
-              let reply = res.results[0].values.text;
-              io.to(socket.id).emit('botsay', reply);
-              genvoice(reply);
-          })
-          .catch((err) => {
-            console.error(err)
-          })
-        }
-      })
-      .catch(function (err) {
-        console.error(err)
+
+  let pcm_buffer = undefined;
+
+  socket.on('record', function(toggle){
+
+    console.log(toggle);
+    
+    if (toggle == 'on') {
+      pcm_buffer = Buffer.alloc(0);
+      audioRecorder.start().stream().on('data', function(buff) {
+        pcm_buffer = Buffer.concat([pcm_buffer, buff]);
       });
+    }
+    else
+    {
+      audioRecorder.stop();
+
+      client.recognize(pcm_buffer, 'wav', 16000, { lan: 'zh' })
+        .then(function(res) {
+
+          if (res.err_no === 0) {
+            let msg = res.result[0];
+            io.to(socket.id).emit('yousay', msg);
+            robochat(msg)
+              .then((res) => {
+                let reply = res.results[0].values.text;
+                io.to(socket.id).emit('botsay', reply);
+                genvoice(reply);
+            })
+            .catch((err) => {
+              console.error(err)
+            })
+          }
+        })
+        .catch(function (err) {
+          console.error(err)
+        });
+
+    }
+
+  })
+
+  socket.on('yousay', function(){
+    
   });
 });
 
-https.listen(3000, function () {
-  console.log('listening on port 3000! Go to https://localhost:3000/')
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
