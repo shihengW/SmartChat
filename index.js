bf33a23c2bdcc30c997f7eb7ade6fcb638521a92
swei <shiheng.wei@hotmail.com>
2019-05-25 20:29:13 +0800

    Emit to all

--- index.js
+++ index.js
@@ -8,17 +8,19 @@ let reply = require('./modules/chat-service').reply;
 let rec   = require('./modules/recorder'    ).recorder();
 
 io.on('connection', function(socket) {
-    socket.on('record', function(){
+
+    socket.on('record', function() {
         // voice -> text -> reply -> voice -> play
         let process = pcm => {
             if (pcm) {
+                io.emit('record complete');
                 hear(pcm, (res) => {
                     const msg = res.result[0];
-                    io.to(socket.id).emit('yousay', msg);
+                    io.emit('yousay', msg);
 
                     reply(msg, res => {
                         let reply = res.results[0].values.text;
-                        io.to(socket.id).emit('botsay', reply);
+                        io.emit('botsay', reply);
                         read(reply);
                     });
                 });
@@ -30,7 +32,6 @@ io.on('connection', function(socket) {
 
         rec.start()
         .on('close', () => {
-            io.to(socket.id).emit('record complete');
             process(rec.complete());
         });
     });
